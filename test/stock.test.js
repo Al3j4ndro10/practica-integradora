d7a47de23ccafc6c7fe9963bb6c78ab2061d024e
Al3j4ndro10 <amaquilonchavarro@gmail.com>
2025-12-19 00:24:06 -0500

    Update import path for calcularStock function

--- test/stock.test.js
+++ test/stock.test.js
@@ -1,4 +1,4 @@
-const { calcularStock } = require('../src/services/inventarioService');
+const { calcularStock } = require('../src/services/stock');
 
 test('calcularStock retorna el stock correcto', () => {
   expect(calcularStock(10, 5, 3)).toBe(12);
